0a3b8ee57c6d92a6f626db9c4700d5b8f080df5b
EnaSackey01 <enas.sackey@outlook.com>
2023-04-01 19:16:26 -0400

    recovery code

--- extract.py
+++ extract.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 import pandas as pd
+import functools as ft
 
 #Establishing initial connection to Amazon RDS
 engine = create_engine(connection)
@@ -19,12 +20,25 @@ skills = Base.classes.skills
 session = Session(engine)
 engine.dispose()
 
-#Building queries to pull all Amazon Data using the "Jobs_result" query)"
+#Building queries to pull all Amazon data using the "Jobs_result" query)"
 jobs_result = session.query(jobs)
 skills_result = session.query(skills)
 salaries_result = session.query(salaries)
 
-#Saving query into Dataframes
-jobs_df = pd.read_sql(jobs_result.statement, engine)
-skills_df = pd.read_sql(skills_result.statement, engine)
-salaries_df = pd.read_sql(salaries_result.statement, engine)
+#Saving query into data frames
+jobs_df = pd.read_sql(sql = jobs_result.statement, con = engine.connect())
+skills_df = pd.read_sql(sql = skills_result.statement, con = engine.connect())
+salaries_df = pd.read_sql(sql = salaries_result.statement, con = engine.connect())
+
+# OP1 - Creating code to join all 3 tables together on their primary key
+#dfs = [jobs_df, skills_df, salaries_df]
+#df_final = ft.reduce(lambda left: pd.merge(left, right, on= 'ID'), dfs) or df_merged = ft.reduce (lambda left, right: pd.merge(left, right, on =['id'], how ='outer'),dfs)
+
+#OP2 - Make two merges
+m1 = pd.merge(salaries_df, jobs_df, how = "inner", on = ["id"])
+tot_merge = pd.merge(m1, skills_df, how = "inner", on = ["id"])
+
+#Writing this join dataframe to the "Data" folder
+tot_merge.to_csv("Data/Joined_data.csv", index = False)
+
+
